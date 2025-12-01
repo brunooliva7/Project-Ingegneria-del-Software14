54d09e0c0320fceac81e21a1cc75354d1a237d92
maramariano <maramariano@localhost>
2025-12-01 19:25:56 +0100

    Merge origin/master
    Conflicts:
    	Gruppo14BibliotecaUniversitaria/src/main/java/it/unisa/diem/oop/gruppo14bibliotecauniversitaria/Book.java

--- Gruppo14BibliotecaUniversitaria/src/main/java/it/unisa/diem/oop/gruppo14bibliotecauniversitaria/BookManagement.java
+++ Gruppo14BibliotecaUniversitaria/src/main/java/it/unisa/diem/oop/gruppo14bibliotecauniversitaria/BookManagement.java
@@ -5,10 +5,42 @@
  */
 package it.unisa.diem.oop.gruppo14bibliotecauniversitaria;
 
+import java.util.TreeSet;
+
 /**
  *
  * @author maramariano
  */
-public class BookManagement {
+public class BookManagement implements Functionality {
+    private TreeSet<Book> catalog;
+    
+    public BookManagement() {
+        this.catalog = new TreeSet<>();
+    }
+    
+    @Override
+    public void add(Book b) {
+        
+    }
+    
+    @Override
+    public void remove(Book b) {
+        
+    }
+    
+    @Override
+    public void update(Book b){
+        
+    }
+    
+    @Override
+    public void viewSorted(){
+        
+    }
+    
+    @Override
+    public Book search(Book b){
+        
+    }
     
 }
