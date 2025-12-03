f37a94db5e3b7880ae270407654a88a5f29dc39b
elisa <elisa@LAPTOP-05MSAVKC>
2025-12-03 18:03:46 +0100

    Merge origin/master
    scheletri e
    
    	Gruppo14BibliotecaUniversitaria/src/main/java/it/unisa/diem/oop/gruppo14bibliotecauniversitaria/Book.java

--- Gruppo14BibliotecaUniversitaria/src/main/java/it/unisa/diem/oop/gruppo14bibliotecauniversitaria/control/Control.java
+++ Gruppo14BibliotecaUniversitaria/src/main/java/it/unisa/diem/oop/gruppo14bibliotecauniversitaria/control/Control.java
@@ -10,5 +10,19 @@ package it.unisa.diem.oop.gruppo14bibliotecauniversitaria.control;
  * @author maramariano
  */
 public class Control {
+     Model model;
+      View view;
     
+     public Controller(Model model,View view){
+         this.model=model;
+         this.view=view;
+         initBindings();
+         initHandler();
+     }
+     
+      public void initBindings(){
+      // Prima cosa: collego textfield(view) ↔ mode
+      }
+      //metodo che gestisce gli eventi causati dall'interazione tra utente e view
+    public void initHandler(){}
 }
