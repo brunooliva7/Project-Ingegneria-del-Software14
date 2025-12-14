5f07df85bad58f7cb743cb8439ade74119ee11db
bruno <bruno@192.168.1.108>
2025-12-14 21:46:02 +0100

    Controller Loan

---
+++ gruppo14BibliotecaUniversitaria/src/main/java/it/unisa/diem/oop/gruppo14bibliotecauniversitaria/control/addLoanController.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.oop.gruppo14bibliotecauniversitaria.control;
+
+import it.unisa.diem.oop.gruppo14bibliotecauniversitaria.model.data.Book;
+import it.unisa.diem.oop.gruppo14bibliotecauniversitaria.model.data.Loan;
+import it.unisa.diem.oop.gruppo14bibliotecauniversitaria.model.data.User;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import it.unisa.diem.oop.gruppo14bibliotecauniversitaria.model.management.BookManagement;
+import it.unisa.diem.oop.gruppo14bibliotecauniversitaria.model.management.LoanManagement;
+import it.unisa.diem.oop.gruppo14bibliotecauniversitaria.model.management.UserManagement;
+import it.unisa.diem.oop.gruppo14bibliotecauniversitaria.view.View;
+import java.io.IOException;
+import java.time.LocalDate;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author bruno
+ */
+public class addLoanController {
+    
+    @FXML
+    private ComboBox<User> userComboBox;
+    
+    @FXML
+    private ComboBox<Book> bookComboBox;
+    
+    @FXML
+    private DatePicker dueDatePicker;
+    
+    @FXML
+    private Label labelMessage;
+    
+    @FXML
+    private Button registerButton;
+    
+    private UserManagement userManagement;
+    private BookManagement bookManagement;
+    private Book book = null;
+    private User user = null;
+    private LocalDate duedate = null;
+    private Loan loan = null;
+    private LoanManagement loanManagement = null;
+    
+    @FXML
+    private void initialize(){
+        
+        this.userManagement = new UserManagement();
+        this.bookManagement = new BookManagement();
+        
+        ObservableList<User> userList = FXCollections.observableArrayList(userManagement.getList());
+        ObservableList<Book> bookList = FXCollections.observableArrayList(bookManagement.getCatalogue());
+        
+        userComboBox.setItems(userList);
+        bookComboBox.setItems(bookList);
+        
+        registerButton.disableProperty().bind(userComboBox.valueProperty().isNull().or(bookComboBox.valueProperty().isNull().or(dueDatePicker.valueProperty().isNull())));
+    }
+    
+    @FXML
+    private void registerLoan(){
+        
+        this.loanManagement = new LoanManagement();
+        
+         book = bookComboBox.getValue();
+         user = userComboBox.getValue();
+         duedate = dueDatePicker.getValue();
+         
+         loan = new Loan(book,user,duedate);
+         
+         if(loanManagement.add(loan)){
+             labelMessage.setText("Modifica Riuscita");
+             labelMessage.setStyle("-fx-text-fill: green;");
+         }
+         
+         else{
+             labelMessage.setText("Modifica Riuscita");
+             labelMessage.setStyle("-fx-text-fill: green;");
+         }    
+    }
+    
+    @FXML
+    private void backPage() throws IOException{
+        View.Homepage();
+    }
+    
+}
